191c9f5287680561ddf396076c57e05c110c02ca
TECH REWIND EDITZ <149371267+TechRewindEditz@users.noreply.github.com>
2024-12-25 16:28:38 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 # Configuration
 class Config:
-    USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
+    USER_AGENT = "(Google Chrome Android/131.0.6778.200; ios/131.0.6778.154) (Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
     TIMEOUT = 30
     MAX_RETRIES = 3
     CHUNK_SIZE = 8192
